8be16b9aae01c465a3d278e7e41d909939f725ae
CrisMon-01 <cristianmonti1995@gmail.com>
2023-01-19 20:24:08 +0100

    TO DO aggiustare dequeue

---
+++ Queue/queue.py
@@ -0,0 +1,49 @@
+
+
+# Operazioni
+# is_empty(self) -> bool
+# size(self) -> int
+# enqueue(self, dato) -> void
+# dequeue(self) -> bool
+# head(self) -> int 
+# tail(self) -> int
+
+class Queue:
+
+    def __init__(self):
+        self.size = 0
+        self.head = None
+        self.tail = None
+        self.internal_queue = []
+
+    def is_empty(self):
+        return self.size == 0
+
+    def the_size(self):
+        return(len(self.internal_queue))
+
+    def enqueue(self, dato):
+        if not self.head:
+            self.head = dato
+        self.tail = dato
+        self.size += 1
+        self.internal_queue.append(dato)
+        return 
+
+    def dequeue(self):
+        if self.size <=0 :
+            return False
+        else:
+            self.size -= 1
+            self.internal_queue.pop()
+            self.head = self.internal_queue[self.size-1]
+            return True
+
+    def the_head(self):
+        head = self.head if self.head else -1
+        return head
+    
+    def the_tail(self):
+        tail = self.tail if self.tail else -1
+        return tail
+
---
+++ queue.py
@@ -0,0 +1,37 @@
+from Queue.queue import Queue
+
+queue_test = Queue()
+print(queue_test.the_head())
+print(queue_test.the_tail())
+print(queue_test.the_size())
+print(queue_test.is_empty())
+
+queue_test.enqueue(5)
+print(queue_test.the_head())
+print(queue_test.the_tail())
+print(queue_test.the_size())
+print(queue_test.is_empty())
+
+queue_test.enqueue(3)
+print(queue_test.the_head())
+print(queue_test.the_tail())
+print(queue_test.the_size())
+print(queue_test.is_empty())
+
+queue_test.dequeue()
+print(queue_test.the_head())
+print(queue_test.the_tail())
+print(queue_test.the_size())
+print(queue_test.is_empty())
+
+queue_test.dequeue()
+print(queue_test.the_head())
+print(queue_test.the_tail())
+print(queue_test.the_size())
+print(queue_test.is_empty())
+
+queue_test.dequeue()
+print(queue_test.the_head())
+print(queue_test.the_tail())
+print(queue_test.the_size())
+print(queue_test.is_empty())
